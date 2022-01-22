61c0658d63dfc0ab5dc1eaa7ccd018c655b5f8b1
Alejandro <alexruiz@uma.es>
2022-01-22 13:20:51 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header">        
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hola mundo
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=mbeC2w-uIEU"
           target="_blank"
           rel="noopener noreferrer"
         >
