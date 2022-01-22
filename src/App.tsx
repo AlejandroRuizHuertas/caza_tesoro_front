5e5bde76bd5e9cceadfdc129ef3eabf6320f63ca
Alejandro <alexruiz@uma.es>
2022-01-22 13:32:53 +0100

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Pincha para saber el secreto de la vida
         </a>
       </header>
     </div>
