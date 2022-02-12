ed28c39f1a9a68a95d50fb1313bc50b1f5fb132f
Miguel Torres Gómez <47118018+mtGomez@users.noreply.github.com>
2022-02-12 18:55:19 +0100

    Añadida funcionalidad de marcadores y área
    Primero tienes que meter el área y luego los tesoros, es decir, no deja meter tesoros si no hay área primero dibujada. 
    Tampoco se pueden meter tesoros fuera del área dibujada y no se puede crear más de un área.
    Se ha añadido un icono al marcador cuando se dibuja.

--- src/components/CreateGame.tsx
+++ src/components/CreateGame.tsx
@@ -1,5 +1,5 @@
 import { height } from "@mui/system";
-import { LatLngExpression } from "leaflet";
+import { circleMarker, LatLngExpression, } from "leaflet";
 import L from "leaflet";
 import React from "react";
 import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from "react-leaflet";
@@ -26,6 +26,24 @@ export const Create = (): JSX.Element => {
   var bufferLong = distanceLong * 0.05;
   const zoom = 4;
 
+  function isMarkerInsidePolygon(marker, poly) {
+    var inside = false;
+    var x = marker.getLatLng().lat, y = marker.getLatLng().lng;
+    for (var ii=0;ii<poly.getLatLngs().length;ii++){
+        var polyPoints = poly.getLatLngs()[ii];
+        for (var i = 0, j = polyPoints.length - 1; i < polyPoints.length; j = i++) {
+            var xi = polyPoints[i].lat, yi = polyPoints[i].lng;
+            var xj = polyPoints[j].lat, yj = polyPoints[j].lng;
+
+            var intersect = ((yi > y) != (yj > y))
+                && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+            if (intersect) inside = !inside;
+        }
+    }
+
+    return inside;
+  };  
+
   function MyComponent() {
     const map = useMapEvents({
       click: (e) => {
@@ -36,6 +54,15 @@ export const Create = (): JSX.Element => {
       },
 
     })
+
+    var customMarker= L.Icon.extend({
+      options: {
+          shadowUrl: null,
+          iconAnchor: new L.Point(24, 24),
+          iconSize: new L.Point(48, 48),
+          iconUrl: 'https://cdn.pixabay.com/photo/2014/12/21/23/27/treasure-chest-575386_960_720.png'
+      }
+  });
 
     var drawnItems = new L.FeatureGroup();
     map.addLayer(drawnItems);
@@ -48,21 +75,49 @@ export const Create = (): JSX.Element => {
           circlemarker: false,
           polygon: false,
           polyline: false,
-          marker: false,
-        }
+          marker: {
+            icon: new customMarker()
+          },
+        },
+        
     });
 
     var playableArea,p1,p2,p3,p4;
+    var areaLayer;
 
     map.addControl(drawControl);
     map.on(L.Draw.Event.CREATED, function(e){
-      map.addLayer(e.layer);
-      p1 = "["+e.layer.getLatLngs()[0][0]["lat"]+","+e.layer.getLatLngs()[0][0]["lng"]+"]"
-      p2 = "["+e.layer.getLatLngs()[0][1]["lat"]+","+e.layer.getLatLngs()[0][1]["lng"]+"]"
-      p3 = "["+e.layer.getLatLngs()[0][2]["lat"]+","+e.layer.getLatLngs()[0][2]["lng"]+"]"
-      p4 = "["+e.layer.getLatLngs()[0][3]["lat"]+","+e.layer.getLatLngs()[0][3]["lng"]+"]"
-      playableArea = "["+p1+","+p2+","+p3+","+p4+"]"
-      console.log(playableArea);
+      drawnItems.addLayer(e.layer);
+      if (e.layer instanceof L.Rectangle){
+        if(!playableArea){
+          p1 = "["+e.layer.getLatLngs()[0][0]["lat"]+","+e.layer.getLatLngs()[0][0]["lng"]+"]";
+          p2 = "["+e.layer.getLatLngs()[0][1]["lat"]+","+e.layer.getLatLngs()[0][1]["lng"]+"]";
+          p3 = "["+e.layer.getLatLngs()[0][2]["lat"]+","+e.layer.getLatLngs()[0][2]["lng"]+"]";
+          p4 = "["+e.layer.getLatLngs()[0][3]["lat"]+","+e.layer.getLatLngs()[0][3]["lng"]+"]";
+          playableArea = "["+p1+","+p2+","+p3+","+p4+"]";
+          areaLayer = e.layer;
+        }else{
+          e.layer.remove();
+        }
+      }
+      
+      if (e.layer instanceof L.Marker){
+        if(playableArea){
+          if(isMarkerInsidePolygon(e.layer,areaLayer)){
+            console.log("YAI");
+          }else{
+            e.layer.remove();
+          }
+        }else{
+          e.layer.remove();
+        }
+      }
+    
+    });
+
+    map.on(L.Draw.Event.DELETED, function(e){
+      playableArea = null;
+      areaLayer = null;
     });
 
     //var coords = "[[33.49674296343329, 93.99902343750001],[38.67800554026613, 93.99902343750001],[38.67800554026613, 98.30566406250001],[33.49674296343329, 98.30566406250001]]"
